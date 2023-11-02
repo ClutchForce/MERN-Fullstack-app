390babba2717a7ca86e7a7054531223cd1cacdab
Paul Gherghel <pagherghel@yahoo.com>
2023-11-02 00:55:21 -0400

    implemented superhero data retrieval APIs

--- server/server.js
+++ server/server.js
@@ -2,6 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const fs = require('fs');
+
+function readSuperheroInfo() {
+  const data = fs.readFileSync(path.join(__dirname,'../superhero_info.json'), 'utf-8');
+  return JSON.parse(data);
+}
+
+function readSuperheroPowers() {
+    const data = fs.readFileSync(path.join(__dirname,'../superhero_powers.json'), 'utf-8');
+    return JSON.parse(data);
+  }
+  
 
 app.use(express.static(path.join(__dirname, '../client')));
 
@@ -12,3 +24,26 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+app.get('/api/superheroes/:id', (req, res) => {
+    const superheroes = readSuperheroInfo();
+    const superhero = superheroes.find(hero => hero.id === parseInt(req.params.id));
+    
+    if (!superhero) {
+      return res.status(404).send('Superhero not found');
+    }
+  
+    res.json(superhero);
+});
+
+app.get('/api/superheroes/:id/powers', (req, res) => {
+    const superheroPowers = readSuperheroPowers();
+    const powers = superheroPowers[req.params.id];
+    
+    if (!powers) {
+      return res.status(404).send('Powers not found for the given superhero ID');
+    }
+  
+    res.json(powers);
+  });
+  
